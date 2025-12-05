7ab770b10b19b207ce47e1ee0f90ee21b95abf32
JoseGvBarrero <152676471+JoseGvBarrero@users.noreply.github.com>
2025-12-05 21:17:45 +0100

    Update Problema1.py

--- Problema1.py
+++ Problema1.py
@@ -1,4 +1,23 @@
-numero1 = 2
-numero2 = 4
-numero3 = numero1 + numero2
-print(numero3)
+def listaRecibida(listaNumeros):
+  listaSinNegativos[]
+  #Recorremos los numeros de la lista
+  for num in listaNumeros:
+    #Si es negativo no lo incluimos, si es positivo agregamos
+    if num > 0:
+      listaSinNegativos.add(num)
+
+  #Quitamos duplicados con Set
+  listaSinDuplicados = set(listaSinNegativos)
+  #Ordenamos
+  listaFinal = sorted(listaSinDuplicados)
+  #Devolvemos la lista actualizada
+  return listaFinal
+
+lista = [1, 25, -5, 2, 3, -3 , 2, 3, 8, 12, 11]
+resultado = listaRecibida(lista)
+print("Lista original: ", lista)
+print("Lista actualizada: ", resultado)
+
+
+  
+  
