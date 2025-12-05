9fdc767dbb2f9c94897ae5356e9d50730be7aca9
JoseGvBarrero <152676471+JoseGvBarrero@users.noreply.github.com>
2025-12-05 20:36:41 +0000

    modified:   Problema1.py

--- Problema1.py
+++ Problema1.py
@@ -15,7 +15,7 @@ def listaRecibida(listaNumeros):
 
 lista = [1, 25, -5, 2, 3, -3 , 2, 3, 8, 12, 11]
 resultado = listaRecibida(lista)
-#Imprimimos ambos resultados para ver la diferencia
+#Imprimimos ambos resultados para ver la diferencia entre ellos
 print("Lista original: ", lista)
 print("Lista actualizada: ", resultado)
 
